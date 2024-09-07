5c4154349e075845375409e355e718fff75e1039
Laufer Artyom <lauferartyom@vk.com>
2024-09-07 19:05:34 +0700

    Some tests have been added (hotfix)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
+    //develop
+    static int[] arr = {4, 1, 2, 32, 42, 2, 42, 92};
+
+    static public int[] getSortArray() {
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    static public boolean IsSorted(){
+        int []_arr = getSortArray();
+        for (int i = 1; i < _arr.length; i++){
+            if(_arr[i-1] > _arr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static public int findMax(){
+        return Arrays.stream(arr).max().getAsInt();
+    }
+
+    static public int findMin(){
+        return Arrays.stream(arr).min().getAsInt();
+    }
 }
