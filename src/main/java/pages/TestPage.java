b7d0f5027726f7eda714a05aabc138edf8374002
Laufer Artyom <lauferartyom@vk.com>
2024-09-15 16:33:33 +0700

    added TestPAge class and some base operations

--- src/main/java/pages/TestPage.java
+++ src/main/java/pages/TestPage.java
@@ -1,4 +1,28 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class TestPage {
+    SelenideElement projectBtn = $x("//a[@id='browse_link']");
+    SelenideElement testBtn = $x("//a[@id='admin_main_proj_link_lnk']");
+    SelenideElement sum = $x("//div[@class='showing']");
+    SelenideElement createTask = $x("//div[@class='iic-trigger']//button[@class='aui-button aui-button-subtle']");
+
+    public void clickBtn(){
+        projectBtn.click();
+        testBtn.click();
+    }
+
+    public void checkTask(){
+        String sSum = sum.getText();
+        System.out.println(sSum);
+        createTask.click();
+        $(By.xpath("//div[@class='iic-widget']")).should(Condition.visible);
+        $(By.xpath("//textarea[@name='summary']")).setValue("fix bag").pressEnter();
+    }
 }
--- src/test/java/WebHookTest.java
+++ src/test/java/WebHookTest.java
@@ -1,14 +1,23 @@
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
+import pages.TestPage;
 import webhooks.WebHook;
 
-    public class WebHookTest extends WebHook {
-    private LoginPage lPage = new LoginPage();
+public class WebHookTest extends WebHook {
+    private final LoginPage lPage = new LoginPage();
+    private final TestPage tPage = new TestPage();
+
 
     @Test
     public void edujiraIFTest(){
         lPage.inputLogin("AT6");
         lPage.inputPassword("Qwerty123");
         lPage.clickBtn();
+
+        tPage.clickBtn();
+        tPage.checkTask();
     }
+
+
+
 }
