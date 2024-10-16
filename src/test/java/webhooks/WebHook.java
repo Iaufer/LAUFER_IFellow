4625107fa46d3468b29ce15c804ee9f9e05ca90a
Artyom <lauferartyom@vk.com>
2024-10-16 14:20:42 +0700

    Update WebHook.java

--- src/test/java/webhooks/WebHook.java
+++ src/test/java/webhooks/WebHook.java
@@ -1,37 +1,3 @@
-//package webhooks;
-//
-//import ConfigManager.ConfigManager;
-//import com.codeborne.selenide.Configuration;
-//import com.codeborne.selenide.Selenide;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.PageLoadStrategy;
-//
-//
-//
-//public class WebHook {
-//    private final String URL = ConfigManager.getUrl();
-//
-//    @BeforeEach
-//    public void initBrowser(){
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--start-maximized");
-//        Configuration.browserSize = null;
-//        Configuration.browserCapabilities = options;
-//        Configuration.pageLoadStrategy = PageLoadStrategy.NORMAL.toString();
-//        Configuration.timeout = 20000;
-//        Selenide.open(URL);
-//    }
-//
-//
-//    @AfterEach
-//    public void tearDown() {
-//        Selenide.closeWebDriver();
-//    }
-//}
-
 package webhooks;
 
 import ConfigManager.ConfigManager;
